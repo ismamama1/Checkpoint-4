dbc80689b74fbf5c43ca0ddf1a8ea13af59b1321
ismamamama1 <ismael.agb@hotmail.fr>
2022-07-20 15:10:49 +0200

    Setup custom colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      black: "#24262E",
+      beige: "#EDE0D4",
+      grey: "#9A96B0",
+      green: "#84F877",
+    },
     fontFamily: {
       heading: ["Libre Baskerville", "serif"],
       body: ["Hind", "sans-serif"],
