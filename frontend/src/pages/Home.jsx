3d28cee580ffe9015f7a87d5633d837e30ccbe0c
ismamamama1 <ismael.agb@hotmail.fr>
2022-07-21 01:32:18 +0200

    Set slots and positions for images

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,19 +6,16 @@ function Home() {
   return (
     <div className="h-max bg-black">
       <section className="flex flex-col justify-between gap-y-28 p-16">
-        <div className="justify-start">
-          <img src="" alt="vinyl" className="h-full" />
-          <h1 className="text-left font-heading text-grey text-3xl md:text-4xl lg:text-4xl md:mr-[26rem] md:ml-[6rem] lg:mr-[46rem] lg:ml-[16rem] pt-6 md:pt-16">
-            Contribute together and bring alive what is missing with streaming
-            platforms.
-          </h1>
-        </div>
-        <div className="justify-end">
-          <img src="" alt="cd" className="h-full" />
-          <h2 className="text-right font-heading text-grey text-3xl md:text-4xl lg:text-4xl md:ml-[28rem] md:mr-[6rem] lg:ml-[46rem] lg:mr-[16rem] pb-6 md:pb-16">
-            Get the magic back to what it was.
-          </h2>
-        </div>
+        <img src="" alt="vinyl" className="absolute inset-y-80 left-40" />
+        <h1 className="text-left font-heading text-grey text-3xl md:text-4xl lg:text-4xl md:mr-[26rem] md:ml-[6rem] lg:mr-[46rem] lg:ml-[16rem] pt-6 md:pt-16">
+          Contribute together and bring alive what is missing with streaming
+          platforms.
+        </h1>
+        <img src="" alt="cd" className="absolute inset-y-full right-40" />
+        <h2 className="text-right font-heading text-grey text-3xl md:text-4xl lg:text-4xl md:ml-[28rem] md:mr-[6rem] lg:ml-[46rem] lg:mr-[16rem] pb-6 md:pb-16">
+          Get the magic back to what it was.
+        </h2>
+
         <div className="flex justify-center mb-9 md:mb-20">
           <Button content="Discover" link="" />
         </div>
