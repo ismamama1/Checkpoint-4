90003750952dc2e182a44d0f9a34ede3287c2954
ismamamama1 <ismael.agb@hotmail.fr>
2022-07-20 15:04:25 +0200

    Fonts setup

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      heading: ["Libre Baskerville", "serif"],
+      body: ["Hind", "sans-serif"],
+    },
   },
   plugins: [],
 };
