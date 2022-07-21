649c87b1f706a1fe33332657fb922b2909596cc8
ismamamama1 <ismael.agb@hotmail.fr>
2022-07-21 16:51:39 +0200

    Created filters section, disabled no prop spreading rule

--- frontend/src/pages/List.jsx
+++ frontend/src/pages/List.jsx
@@ -1,7 +1,73 @@
 import React from "react";
 
+import { TextField, Autocomplete } from "@mui/material";
+
+const options = [
+  { label: "The Shawshank Redemption", year: 1994 },
+  { label: "The Godfather", year: 1972 },
+  { label: "The Godfather: Part II", year: 1974 },
+  { label: "The Dark Knight", year: 2008 },
+  { label: "12 Angry Men", year: 1957 },
+];
+
 function List() {
-  return <div>List</div>;
+  return (
+    <div>
+      <div className="h-70 bg-black flex flex-col items-center md:flex-row justify-center">
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo-1"
+          options={options}
+          // getOptionLabel={(option) => option.title.replace("&apos;E", "'É")}
+          sx={{
+            width: 300,
+            mb: 1,
+            "@media screen and (min-width: 48em)": {
+              mr: 1,
+            },
+          }}
+          // onChange={(e, diplome) => setDiplome(diplome.id)}
+          renderInput={(params) => (
+            <TextField {...params} label="Diplôme" color="primary" />
+          )}
+        />
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo-2"
+          options={options}
+          // getOptionLabel={(option) => option.job.replace("&apos;E", "'É")}
+          sx={{
+            width: 300,
+            mb: 1,
+            "@media screen and (min-width: 48em)": {
+              mr: 1,
+            },
+          }}
+          // onChange={(e, profession) => setProfession(profession.id)}
+          renderInput={(params) => (
+            <TextField {...params} label="Profession" color="primary" />
+          )}
+        />
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo-3"
+          options={options}
+          // getOptionLabel={(option) => option.year}
+          sx={{
+            width: 300,
+            mb: 1,
+            "@media screen and (min-width: 48em)": {
+              mr: 1,
+            },
+          }}
+          // onChange={(e, years) => setYears(years.id)}
+          renderInput={(params) => (
+            <TextField {...params} label="Année" color="primary" />
+          )}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default List;
