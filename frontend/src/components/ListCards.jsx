8a0d2e2815e6f73d9d16dd58bc170976ffe0a4a1
ismamamama1 <ismael.agb@hotmail.fr>
2022-07-22 13:02:28 +0200

    Fetched dummy API

--- frontend/src/components/ListCards.jsx
+++ frontend/src/components/ListCards.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 
-function ListCards() {
+function ListCards({ image, name, house }) {
   return (
     <div className="flex flex-row h-screen w-screen justify-between bg-black">
       <div className="w-[45%] h-[40%] md:w-[22%] lg:w-[18%] lg:h-[40%] xl:w-[15%] bg-beige rounded overflow-hidden shadow-lg m-8 p-2 text center">
-        <img className="w-full" src="" alt="cover" />
+        <img className="w-[30%]" src={image} alt={name} loading="lazy" />
         <div className="bg-grey rounded shadow-lg px-6 py-4">
           <div className="text-black font-heading font-semibold text-xl mb-2">
             Title
           </div>
-          <div className="text-black font-heading text-xl mb-2">Artist</div>
-          <p className="text-black text-body">Genre</p>
+          <div className="text-black font-heading text-xl mb-2">
+            Artist: {name}
+          </div>
+          <p className="text-black text-body">Genre: {house}</p>
           <p className="text-black text-body">Year</p>
         </div>
         <div className="px-6 pt-4 pb-2">
--- frontend/src/pages/List.jsx
+++ frontend/src/pages/List.jsx
@@ -1,14 +1,39 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
+import axios from "axios";
 
 import Filters from "@components/Filters";
 import ListCards from "@components/ListCards";
 
 function List() {
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "http://hp-api.herokuapp.com/api/characters",
+    }).then((response) => {
+      setCharacters(response.data.slice(1, 20));
+      setLoading(false);
+    });
+  }, []);
+
   return (
-    <div>
-      <Filters />
-      <ListCards />
-    </div>
+    <>
+      {loading && <h1>Loading ...</h1>}
+      {!loading && (
+        <div>
+          <Filters />
+          {characters.map((character) => (
+            <ListCards
+              image={character.image}
+              name={character.name}
+              house={character.house}
+            />
+          ))}
+        </div>
+      )}
+    </>
   );
 }
 
