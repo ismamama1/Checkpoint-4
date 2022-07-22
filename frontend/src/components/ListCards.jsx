02bea4342f79376ad4c215a629b2d871be11dd3a
ismamamama1 <ismael.agb@hotmail.fr>
2022-07-22 11:20:29 +0200

    Card CSS adjustments

--- frontend/src/components/ListCards.jsx
+++ frontend/src/components/ListCards.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function ListCards() {
   return (
-    <div className="flex h-screen w-screen justify-between bg-black">
-      <div className="flex flex-row w-[45%] h-[40%] md:w-[22%] lg:w-[18%] lg:h-[40%] xl:w-[15%] bg-beige rounded overflow-hidden shadow-lg p-8 text center">
+    <div className="flex flex-row h-screen w-screen justify-between bg-black">
+      <div className="w-[45%] h-[40%] md:w-[22%] lg:w-[18%] lg:h-[40%] xl:w-[15%] bg-beige rounded overflow-hidden shadow-lg m-8 p-2 text center">
         <img className="w-full" src="" alt="cover" />
-        <div className="px-6 py-4">
+        <div className="bg-grey rounded shadow-lg px-6 py-4">
           <div className="text-black font-heading font-semibold text-xl mb-2">
             Title
           </div>
@@ -14,7 +14,7 @@ function ListCards() {
           <p className="text-black text-body">Year</p>
         </div>
         <div className="px-6 pt-4 pb-2">
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-body text-black mr-2 mb-2">
+          <span className="inline-block rounded shadow px-3 py-1 text-sm font-semibold text-body text-black mr-2 mb-2">
             Media
           </span>
         </div>
