34c0910dd9fb61c5d6f1f359b5d3c1b7f2dcf1d4
ismamamama1 <ismael.agb@hotmail.fr>
2022-07-24 20:07:48 +0200

    Created header and card

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Register() {
-  return <div>Register</div>;
+  return (
+    <div className="flex flex-col items-center h-full w-full bg-black">
+      <h1 className="text-grey text-4xl font-heading mt-14">Register</h1>
+      {/* Background Card */}
+      <div className="flex flex-col h-[90%] w-[80%] md:w-[22%] lg:w-[18%] xl:w-[40%] rounded-md shadow-lg bg-beige m-6 xl:m-12">
+        hahaha
+      </div>
+    </div>
+  );
 }
 
 export default Register;
