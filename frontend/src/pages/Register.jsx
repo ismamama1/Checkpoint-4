d15c10146ad21050523d1c455b6e40fcb3867486
ismamamama1 <ismael.agb@hotmail.fr>
2022-07-24 22:09:09 +0200

    Register page clean

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 
+import Button from "@components/Button";
 import { TextField } from "@mui/material";
 
 function Register() {
   return (
-    <div className="flex flex-col items-center h-screen w-full bg-black">
-      <h1 className="text-grey text-4xl font-heading mt-14">Register</h1>
+    <div className="flex flex-col items-center h-full w-full bg-black">
+      <h1 className="text-grey text-4xl font-heading mt-8 md:mt-14 p-5">
+        Create your profile
+      </h1>
       {/* Background Card */}
       <div className="flex flex-col h-[90%] w-[80%] md:w-[22%] lg:w-[18%] xl:w-[40%] rounded-md shadow-lg bg-beige m-6 xl:m-12">
-        <div className="flex flex-col justify-center p-8">
+        <div className="flex flex-col items-center gap-y-4 p-8 mt-8">
           <TextField
             // {...field}
             label="Username"
             size="medium"
+            variant="standard"
+            sx={{
+              width: 250,
+              mb: 1,
+              "@media screen and (min-width: 48em)": {
+                width: 300,
+              },
+            }}
             // error={errors && errors.lastname}
             // helperText={
             //   errors.lastname?.type === "required" &&
@@ -24,6 +35,14 @@ function Register() {
             // {...field}
             label="Email"
             size="medium"
+            variant="standard"
+            sx={{
+              width: 250,
+              mb: 1,
+              "@media screen and (min-width: 48em)": {
+                width: 300,
+              },
+            }}
             // error={errors && errors.lastname}
             // helperText={
             //   errors.lastname?.type === "required" &&
@@ -36,6 +55,14 @@ function Register() {
             label="Password"
             size="medium"
             type="password"
+            variant="standard"
+            sx={{
+              width: 250,
+              mb: 1,
+              "@media screen and (min-width: 48em)": {
+                width: 300,
+              },
+            }}
             // error={errors && errors.lastname}
             // helperText={
             //   errors.lastname?.type === "required" &&
@@ -48,6 +75,14 @@ function Register() {
             label="Confirm password"
             size="medium"
             type="password"
+            variant="standard"
+            sx={{
+              width: 250,
+              mb: 1,
+              "@media screen and (min-width: 48em)": {
+                width: 300,
+              },
+            }}
             // error={errors && errors.lastname}
             // helperText={
             //   errors.lastname?.type === "required" &&
@@ -56,6 +91,12 @@ function Register() {
             // {...register("password", { required: true })}
           />
         </div>
+        <div className="flex justify-end mt-7 mr-14 mb-7">
+          <Button content="Register" link="" />
+        </div>
+      </div>
+      <div className="flex justify-center mb-20 mt-15 md:mb-20">
+        <Button content="Back to list" link="/list" />
       </div>
     </div>
   );
