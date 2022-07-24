ffc2b988fffe2283de76a6db5a7fc9b40e68b8fe
ismamamama1 <ismael.agb@hotmail.fr>
2022-07-24 15:17:40 +0200

    Corrected scrolling memorization

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+import ScrollToTop from "@services/ScrollToTop";
 import { createTheme, ThemeProvider } from "@mui/material";
 
 import App from "./App";
@@ -26,6 +27,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider theme={theme}>
+        <ScrollToTop />
         <App />
       </ThemeProvider>
     </BrowserRouter>
