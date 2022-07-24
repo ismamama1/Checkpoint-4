3fed0b0a685ea5350bcc3bada3b1aadb8aacd89f
ismamamama1 <ismael.agb@hotmail.fr>
2022-07-24 14:26:46 +0200

    Cards clean

--- frontend/src/components/ListCards.jsx
+++ frontend/src/components/ListCards.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-function ListCards({ image, name, house }) {
+function ListCards({ image, name, house, species, yearOfBirth }) {
   const navigate = useNavigate();
 
   const handleClick = () => {
@@ -13,27 +13,29 @@ function ListCards({ image, name, house }) {
       onKeyPress={handleClick}
       role="button"
       tabIndex="0"
-      className="w-[45%] md:w-[22%] lg:w-[18%] xl:w-[18%] flex flex-col flex-wrap rounded-lg shadow-lg bg-beige overflow-hidden m-4 h-auto hover:opacity-90"
+      className="max-w-sm flex flex-col flex-wrap rounded-lg shadow-lg bg-beige overflow-hidden m-4 h-auto hover:opacity-90"
     >
       {/* <div className="w-[45%] h-[80%] md:w-[22%] lg:h-[50%] lg:w-[90%] xl:w-[15%] bg-beige rounded overflow-hidden m-8 p-2 text center"> */}
 
       <img
-        className="object-contain w-15 h-30 pb-4"
+        className="h-80 w-sm object-cover pb-4"
         src={image}
         alt={name}
         loading="lazy"
       />
-      <div className="bg-grey rounded shadow-lg px-6 py-4 mx-2">
+      <div className="bg-grey bb-52 rounded shadow-lg px-6 py-4 mx-2">
         <div className="text-black font-heading font-semibold text-xl mb-2">
           Title
         </div>
         <div className="text-black font-heading text-lg mb-2 ">
           Artist: {name}
         </div>
-        <p className="text-black text-body">Genre:{house}</p>
-        <p className="text-black text-body">Year</p>
-        <span className="inline-block bg-beige rounded shadow px-3 py-1 text-sm font-semibold text-body text-black mr-2 mb-2 mt-4">
-          Media
+        <p className="text-black text-body">Genre: {house}</p>
+        <p className="text-black text-body">Year: {yearOfBirth}</p>
+      </div>
+      <div className="px-6 pt-4 pb-2">
+        <span className="inline-block bg-beige rounded shadow px-3 py-1 text-sm font-semibold text-body text-black mr-2 mb-2">
+          Media: {species}
         </span>
       </div>
     </div>
--- frontend/src/pages/Contribution.jsx
+++ frontend/src/pages/Contribution.jsx
@@ -2,11 +2,31 @@ import React from "react";
 
 import Button from "@components/Button";
 
-function Contribution() {
+function Contribution({ image, name, house, species, yearOfBirth }) {
   return (
     <div className="flex flex-col items-center h-screen w-screen bg-black">
       <div className="flex flex-col h-[80%] w-[45%] md:w-[22%] lg:w-[18%] xl:w-[40%] flex-wrap rounded-md shadow-lg bg-beige overflow-hidden m-6">
-        blbllb
+        <img
+          className="h-80 w-sm object-cover pb-4"
+          src={image}
+          alt={name}
+          loading="lazy"
+        />
+        <div className="bg-grey bb-52 rounded shadow-lg px-6 py-4 mx-2">
+          <div className="text-black font-heading font-semibold text-xl mb-2">
+            Title
+          </div>
+          <div className="text-black font-heading text-lg mb-2 ">
+            Artist: {name}
+          </div>
+          <p className="text-black text-body">Genre: {house}</p>
+          <p className="text-black text-body">Year: {yearOfBirth}</p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-beige rounded shadow px-3 py-1 text-sm font-semibold text-body text-black mr-2 mb-2">
+            Media: {species}
+          </span>
+        </div>
       </div>
       <div className="flex justify-center mb-20 mt-20 md:mb-20">
         <Button content="Back to list" link="/list" />
